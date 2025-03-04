fef545af4b4ffe488e846b6904828245ce715896
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-03-04 12:10:58 +0530

    [Mohd Ayan Haque] Add .sorting of contacts alphabetically by name

--- src/index.js
+++ src/index.js
@@ -32,11 +32,13 @@ try {
     console.log(addressBook.viewPersonsByCityOrState());
 
     console.log("Count of Contacts by City and State:");
-    console.log(addressBook.countByCityOrState());
+    console.log(addressBook.countByCityOrState()); 
+
+    console.log("\nContacts Sorted Alphabetically:");
+    console.log(addressBook.sortContactsByName().map(contact => contact.toString()).join("\n"));
 } catch (error) {
     console.error("Error:", error.message);
 }
-
 try {
     const invalidContact = new Contact(
         "an", "son", "abc", "In", "I", "100A01", "12345", "invalid-email"
