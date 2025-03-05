42bfb8e99d8e1d5adc68705c3ad5bf4e4e865cd6
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-03-05 11:38:21 +0530

    [Mohd Ayan Haque] Refactor. small changes in index file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Anand", "Soni", "Bhopal", "India", "In", "100001", "9876543210", "anandsoni9165@gmail.com"
+        "ayan", "haque", "Bhopal", "India", "In", "100001", "9876543210", "ayan394i@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
@@ -21,7 +21,7 @@ try {
     console.log("Total Contacts:", addressBook.countContacts()); 
 
     const duplicateContact = new Contact(
-        "Anand", "Soni", "Bhopal", "India", "In", "100001", "9876543210", "anandsoni9165@gmail.com"
+        "ayan", "haque", "Bhopal", "India", "In", "100001", "9876543210", "ayan394i@gmail.com"
     );
     console.log(addressBook.addContact(duplicateContact));  
 
@@ -54,7 +54,7 @@ try {
 }
 try {
     const invalidContact = new Contact(
-        "an", "son", "abc", "In", "I", "100A01", "12345", "invalid-email"
+        "ay", "haq", "abc", "In", "I", "100A01", "12345", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
